03e11e4442c478f35a594e8975897a56236f1473
xhico <francgf@gmail.com>
2023-10-25 17:27:36 +0100

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,5 +2,5 @@
 
 sudo mv /home/pi/ipSender/ipSender.service /etc/systemd/system/ && sudo systemctl daemon-reload
 python3 -m pip install requests yagmail picamera2 numpy lxml --no-cache-dir
-sudo apt install libatlas-base-dev libxslt-dev -y
+sudo apt install libatlas-base-dev libxslt-dev libcap-dev python3-libcamera -y
 chmod +x -R /home/pi/ipSender/*
